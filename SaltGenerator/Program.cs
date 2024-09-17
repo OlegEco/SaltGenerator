dc5d9e0f3771235dc939c74c7f03afb161f9dba1
Oleg <100606637+OlegEco@users.noreply.github.com>
2024-09-17 09:29:18 +0200

    Add project files.

---
+++ SaltGenerator/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine(GenerateSalt());
+
+static string GenerateSalt(int size = 16)
+{
+    var salt = new byte[size];
+    using (var rng = new System.Security.Cryptography.RNGCryptoServiceProvider())
+    {
+        rng.GetBytes(salt);
+    };
+
+    return Convert.ToBase64String(salt);
+}
